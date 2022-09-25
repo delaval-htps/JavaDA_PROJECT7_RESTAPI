4f316c6a33246bf5d1f8c804e55767bbb057634a
Delaval Dorian <delaval.htps@gmail.com>
2022-09-25 14:08:19 +0200

    creation of BidListService

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListService.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot.services;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
@@ -11,6 +12,9 @@ import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.exceptions.BidListNotFoundException;
 import com.nnk.springboot.repositories.BidListRepository;
 
+/**
+ * Service class for {@link BidList}
+ */
 @Service
 public class BidListService {
 
@@ -20,6 +24,13 @@ public class BidListService {
     @Autowired
     private MessageSource messageSource;
 
+    /**
+     * save a {@link BidList} in db.
+     * 
+     * @param bid the bidlist to save
+     * @return the bidlist saved if it was correctly save
+     * @throws BidListNotFoundException if bidList is null
+     */
     public BidList saveBidList(BidList bid) {
         if (bid != null) {
             return bidListRepository.save(bid);
@@ -30,14 +41,22 @@ public class BidListService {
 
     }
 
+    /**
+     * update a Bidlist given in parameter.
+     * 
+     * @param bid the bidlist updated
+     * @return the updated existed bidlist according to bid given in parameter
+     * @throws BidListNotFoundException if bidlist is null or not existant
+     */
     public BidList updateBidList(BidList bid) {
+
         if (bid != null && bid.getBidListId() != 0) {
 
             Optional<BidList> existedBidList = bidListRepository.findById(bid.getBidListId());
-            
+
             if (existedBidList.isPresent()) {
 
-                // TODO save field of bid
+                return bidListRepository.save(bid);
 
             } else {
                 throw new BidListNotFoundException(
@@ -48,6 +67,36 @@ public class BidListService {
             throw new BidListNotFoundException(
                     messageSource.getMessage("global.exception.not-null", new Object[] { "bid" }, Locale.FRANCE));
         }
-        return bid;
+    }
+
+    /**
+     * Retrieve all bidlist in db.
+     * 
+     * @return List of all registred BidList(empty if there were not)
+     */
+    public List<BidList> findBidLists() {
+        return bidListRepository.findAll();
+    }
+
+    /**
+     * Delete a BidList given in parameter.
+     * 
+     * @param bid the bid to delete.
+     * @throws BidListNotFoundException if bid does not exist or if it is null
+     */
+    public void deleteBidList(BidList bid) {
+        if (bid != null && bid.getBidListId() != 0) {
+            Optional<BidList> existedBisList = bidListRepository.findById(bid.getBidListId());
+
+            if (existedBisList.isPresent()) {
+                bidListRepository.delete(existedBisList.get());
+            } else {
+                throw new BidListNotFoundException(
+                        messageSource.getMessage("global.exception.not-found", new Object[] { "bid" }, Locale.FRANCE));
+            }
+        } else {
+            throw new BidListNotFoundException(
+                    messageSource.getMessage("global.exception.not-null", new Object[] { "bid" }, Locale.FRANCE));
+        }
     }
 }
