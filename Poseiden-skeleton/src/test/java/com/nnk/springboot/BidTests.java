24686f6c2b2df9be2ff65f8a31d95617548acc13
Delaval Dorian <delaval.htps@gmail.com>
2022-09-25 14:09:00 +0200

    update BidListTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/BidTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/BidTests.java
@@ -1,16 +1,21 @@
 package com.nnk.springboot;
 
-import com.nnk.springboot.domain.BidList;
-import com.nnk.springboot.repositories.BidListRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Optional;
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.exceptions.BidListNotFoundException;
+import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.services.BidListService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,6 +23,11 @@ public class BidTests {
 
 	@Autowired
 	private BidListRepository bidListRepository;
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	private BidListService cut;
 
 	@Test
 	public void bidListTest() {
@@ -42,5 +52,44 @@ public class BidTests {
 		bidListRepository.delete(bid);
 		Optional<BidList> bidList = bidListRepository.findById(id);
 		Assert.assertFalse(bidList.isPresent());
+	}
+
+	@Test
+	public void bidListTest_whenNullOrNotExist() {
+
+		// Save
+		Assertions.assertThatThrownBy(() -> {
+			cut.saveBidList(null);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("is null");
+
+		// Update
+		Assertions.assertThatThrownBy(() -> {
+			cut.updateBidList(null);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("is null");
+
+		// Delete
+		Assertions.assertThatThrownBy(() -> {
+			cut.deleteBidList(null);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("is null");
+	}
+
+	@Test
+	public void bidListTest_whenNotExist() {
+
+		BidList bid = new BidList();
+		// Save
+		Assertions.assertThatThrownBy(() -> {
+			cut.saveBidList(bid);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("not found");
+
+		// Update
+		Assertions.assertThatThrownBy(() -> {
+			cut.updateBidList(bid);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("not found");
+
+		// Delete
+		Assertions.assertThatThrownBy(() -> {
+			cut.deleteBidList(bid);
+		}).isInstanceOf(BidListNotFoundException.class).hasMessageContaining("not found");
 	}
 }
