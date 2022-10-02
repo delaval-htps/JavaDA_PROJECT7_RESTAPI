933e1845775c21239a0badd1c0afafc89c5e2fd1
Delaval Dorian <delaval.htps@gmail.com>
2022-10-02 21:53:12 +0200

    refresh all project for vscode: change pom.xml and delete .iml

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/RuleTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/RuleTests.java
@@ -1,19 +1,22 @@
 package com.nnk.springboot;
 
-import com.nnk.springboot.domain.RuleName;
-import com.nnk.springboot.repositories.RuleNameRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Optional;
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(value = "test")
 public class RuleTests {
 
 	@Autowired
