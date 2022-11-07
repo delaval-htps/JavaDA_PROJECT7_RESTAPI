afe9bd6c6feddef582527520f6358ec5ba04bcb5
Delaval Dorian <delaval.htps@gmail.com>
2022-11-07 21:50:20 +0100

    First completion of SecurityConfig
    - add SecurityConfig to provide security in application
    - add CustomUserDetailsService to implements DaoAuthenticationProvider
    - implementation of logout ok
    - add custom page for login
    - change href for login in home.html to /app/login to match with
      endpoint of loginController
    - TODO implementation of postMapping "/app/login" to validate the custom
      form login added
    -

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.nnk.springboot.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nnk.springboot.services.CustomUserDetailsService;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.authorizeRequests()
+            
+            .antMatchers("/user/**")
+                .hasRole("ADMIN")
+            
+            .antMatchers("/**")
+                .permitAll()
+               
+            .anyRequest().authenticated();
+
+        http.formLogin()
+            .loginPage("/app/login")
+            .loginProcessingUrl("/app/login")
+            .defaultSuccessUrl("/", true)
+            .permitAll();
+        
+        // configuration for logout OK!
+        http.logout()
+        .logoutUrl("/app-logout")
+        .logoutSuccessUrl("/")
+        .permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.nnk.springboot.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.repositories.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.nnk.springboot.domain.User user = userRepository.findByUsername(username);
+        if (user != null) {
+            List<SimpleGrantedAuthority> userRoles = new ArrayList<>();
+            userRoles.add(new SimpleGrantedAuthority(user.getRole()));
+            return new User(user.getUsername(), user.getPassword(),userRoles);
+        }  else{
+            throw new UsernameNotFoundException(messageSource.getMessage("global.user.not-found",new Object[]{username},LocaleContextHolder.getLocale()));
+        }
+    }
+    
+}
